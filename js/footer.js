cbde027f6a92923549cf404c269f08fa28170a73
dkohn42 <dkohn@pdx.edu>
2022-06-02 14:36:05 -0700

    Home page done, Footer added, accessibility cleaned up

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,16 @@
 let footer = document.getElementById("footer");
 footer.innerHTML = `
-<footer class="bottom text-center align-bottom block">
+<footer class="text-center">
+  <div class="bg-black d-inline-block w-100 text-white">   
+  <a class="btn footer-icon" href="https://github.com/d-kohn">
+    <i class="fa fa-github fa-3x"></i>
+    <span class="visually-hidden">Github</span>
+    </a>                                    
+  <a class="btn footer-icon" href="https://www.linkedin.com/in/darin-kohn-895296239/" target="none">
+    <i class="fa fa-linkedin fa-3x"></i>
+    <span class="visually-hidden">Linkedin</span>
+    </a>
   <p>Created by: Darin Kohn<br>
+  </div>
 </footer> 
 `
--- js/header.js
+++ js/header.js
@@ -1,4 +1,5 @@
 let page_list = ["index", "about", "resume", "projects", "contact"]
+// strip away all URL information except the filename, minus .html
 let currentpage = document.location.href.split("/").pop().split(".")[0];
 console.log(currentpage);
 
@@ -6,33 +7,38 @@ let header = document.getElementById("header");
 header.innerHTML = `
 <header>
   <nav class="navbar navbar-expand navbar-default fixed-top navbar-dark">
-   <div class="container-fluid">
-       <div class="navbar-header">
-         <ul class="navbar-nav">
-           <li class="nav-item">
-             <a class="navbar-brand" href="../index.html">Darin Kohn</a>
-           </li>
-           <li class="nav-item">
-             <a id="about" class="nav-link" href="./about.html">About</a>
-           </li>
-           <li class="nav-item">
-             <a id="resume" class="nav-link" href="./resume.html">Resume</a>
-           </li>
-           <li class="nav-item">
-             <a id="projects" class="nav-link" href="./projects.html">Projects</a>
-           </li>
-           <li class="nav-item">
-             <a id="contact" class="nav-link" href="./contact.html">Contact</a>
-           </li>
-         </ul>
-       </div>
-     <div class="icon">
-       <a class="btn social-icon" href="https://www.linkedin.com/in/darin-kohn-895296239/" target="none">
-         <i class="fa fa-linkedin"></i>
-       </a>
-     </div>
-   </div>
- </nav>
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="navbar-brand" href="../index.html">Darin Kohn</a>
+          </li>
+          <li class="nav-item">
+            <a id="about" class="nav-link" href="./about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a id="resume" class="nav-link" href="./resume.html">Resume</a>
+          </li>
+          <li class="nav-item">
+            <a id="projects" class="nav-link" href="./projects.html">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a id="contact" class="nav-link" href="./contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="icon">
+        <a class="btn navbar-icon" href="https://github.com/d-kohn">
+          <i class="fa fa-github fa-2x"></i>
+          <span class="visually-hidden">Github</span>
+        </a>                                    
+        <a class="btn navbar-icon" href="https://www.linkedin.com/in/darin-kohn-895296239/" target="none">
+          <i class="fa fa-linkedin fa-2x"></i>
+          <span class="visually-hidden">Linkedin</span>
+        </a>
+      </div>
+    </div>
+  </nav>
 </header> 
 `
 if (currentpage === "index") {
