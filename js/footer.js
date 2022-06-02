14df34a4834bedd551d69ac5e769f8b8f0576e55
dkohn42 <dkohn@pdx.edu>
2022-06-02 14:58:11 -0700

    Cleaning up code

--- js/footer.js
+++ js/footer.js
@@ -6,7 +6,7 @@ footer.innerHTML = `
     <i class="fa fa-github fa-3x"></i>
     <span class="visually-hidden">Github</span>
     </a>                                    
-  <a class="btn footer-icon" href="https://www.linkedin.com/in/darin-kohn-895296239/" target="none">
+  <a class="btn footer-icon" href="https://www.linkedin.com/in/darin-kohn-895296239/">
     <i class="fa fa-linkedin fa-3x"></i>
     <span class="visually-hidden">Linkedin</span>
     </a>
--- js/header.js
+++ js/header.js
@@ -32,7 +32,7 @@ header.innerHTML = `
           <i class="fa fa-github fa-2x"></i>
           <span class="visually-hidden">Github</span>
         </a>                                    
-        <a class="btn navbar-icon" href="https://www.linkedin.com/in/darin-kohn-895296239/" target="none">
+        <a class="btn navbar-icon" href="https://www.linkedin.com/in/darin-kohn-895296239/">
           <i class="fa fa-linkedin fa-2x"></i>
           <span class="visually-hidden">Linkedin</span>
         </a>
